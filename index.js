25e1942bfa7fd632a506c7e92228ea357e496b78
gaurabsingharoy <singharoy40@gmail.com>
2026-04-29 18:48:34 +0530

    fix: vercel config for deployment and cors import in index

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const app = express()
 
 app.use(express.json())
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 /*
 const newMeetup = {
     title: "Marketing Seminar: Digital Growth 2026",
